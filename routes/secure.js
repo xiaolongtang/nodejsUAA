1449cc7a642a7ffb4ab0473225caf42c638cc56c
Tang <xiaolong.tang@pactera.com>
2016-08-19 17:15:51 +0800

    first upload

---
+++ routes/secure.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var auth = require('./auth.js');
+var path = require('path');
+var router = express.Router();
+var app = express();
+
+router.use(function(req,res,next){
+  console.log('check for token valid? ' +auth.hasValidSession(req) );
+  if (auth.hasValidSession(req)) {
+      next();
+  } else {
+    next(res.sendStatus(403).send('Forbidden'));
+  }
+});
+
+/* GET Secure resource */
+router.get('/', function(req, res, next) {
+  //console.log('Accessing the secure section ...'+path.join(__dirname + '/secure.html'))
+  res.sendFile(path.join(__dirname + '/../public/secure.html'));
+});
+
+router.get('/token_data', function(req, res, next) {
+  //console.log('Accessing the secure section ...'+path.join(__dirname + '/secure.html'))
+  console.log('Accessing the secure section ...'+auth.getUserToken(req));
+  res.json({"authToken":auth.getUserToken(req)});
+});
+
+/* GET Secure resource for data */
+router.get('/data', function(req, res, next) {
+  console.log('Accessing the secure section ...'+path.join(__dirname + '/secure.html'));
+  res.json(req.app.get('connectedDeviceConfig'));
+});
+
+module.exports = router;
